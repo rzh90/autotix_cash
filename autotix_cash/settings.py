7e1dbe0cd05b3f915fcf96a130f1758e509db246
rzhao28 <arenzi28@outlook.com>
2020-12-30 07:21:04 -0500

    404 page

--- autotix_cash/settings.py
+++ autotix_cash/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '5qf+w%vvl)u5wn$_e8!4a910we9)+^e5t$9r36d#m8cze7owjg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
