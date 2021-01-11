51e2157cd1a5036f8f463e3410c237e6d878f24d
rzhao28 <arenzi28@outlook.com>
2021-01-11 07:00:31 -0500

    Changed URLs to slugs

--- cash/admin.py
+++ cash/admin.py
@@ -6,7 +6,7 @@ from .models import Show, Spent
 # Register your models here.
 class ShowAdmin(admin.ModelAdmin):
     list_display = ('show',)
-    prepopulated_fields = {'slug': ('show',)}
+    # prepopulated_fields = {'slug': ('show',)}
 
 
 admin.site.register(Show, ShowAdmin)
