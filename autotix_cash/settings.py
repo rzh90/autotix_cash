48f4388bf3b44a97d382b325babd9b0b8d67cf27
rzhao28 <arenzi28@outlook.com>
2021-01-14 06:57:39 -0500

    update master

--- autotix_cash/settings.py
+++ autotix_cash/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -116,6 +117,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# My settings
+LOGIN_URL = 'users:login'
 
 # My settings
 LOGIN_URL = 'users:login'
